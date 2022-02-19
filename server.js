6e1eb82ce19f738e9d9f8c309bdfc6fb04252773
Kanboo <kanboowang@17.media>
2022-02-19 23:04:13 +0800

    調整 request header 及 response 回傳格式

--- server.js
+++ server.js
@@ -8,22 +8,27 @@ const REQUEST_METHOD = Object.freeze({
 })
 
 const requestListener = (request, response) => {
-  
+
   const headers = {
-    'Content-Type': 'text-plan'
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
   }
 
   if (request.url === '/' && request.method === REQUEST_METHOD.GET) {
     response.writeHead(200, headers)
-    response.write('Index')
-    response.end()
-  } else if (request.url === '/' && request.method === REQUEST_METHOD.DELETE) {
-    response.writeHead(200, headers)
-    response.write('Delete')
+    response.write(JSON.stringify({
+      status: 'success',
+      data: []
+    }))
     response.end()
   } else {
     response.writeHead(404, headers)
-    response.write('Not found')
+    response.write(JSON.stringify({
+      status: 'false',
+      message: 'Not found'
+    }))
     response.end()
   }
 }
