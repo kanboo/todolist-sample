41c17b5b1064e4dc7eec95c01d75209bfaaabc42
Kanboo <kanboowang@17.media>
2022-02-19 22:49:15 +0800

    新增 首頁 及 404 頁面

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const http = require('http')
 
-const requestListener = (request,response)=>{
-    response.writeHead(200,{'Content-Type':'text-plan'})
-    response.write('Hello guys')
+const REQUEST_METHOD = Object.freeze({
+  GET: 'GET',
+  POST: 'POST',
+  DELETE: 'DELETE',
+  PATH: 'PATH'
+})
+
+const requestListener = (request, response) => {
+  
+  const headers = {
+    'Content-Type': 'text-plan'
+  }
+
+  if (request.url === '/' && request.method === REQUEST_METHOD.GET) {
+    response.writeHead(200, headers)
+    response.write('Index')
     response.end()
+  } else if (request.url === '/' && request.method === REQUEST_METHOD.DELETE) {
+    response.writeHead(200, headers)
+    response.write('Delete')
+    response.end()
+  } else {
+    response.writeHead(404, headers)
+    response.write('Not found')
+    response.end()
+  }
 }
 
 const server = http.createServer(requestListener)
